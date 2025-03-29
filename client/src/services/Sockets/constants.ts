39c6679d047ee2cb3ae9bb05e72c19eb931b41f8
Sanath_ASPL_12 <sanath@aerem.co>
2025-03-29 22:08:39 +0530

    Completed bindings of client mediasoup

--- client/src/services/Sockets/constants.ts
+++ client/src/services/Sockets/constants.ts
@@ -3,4 +3,9 @@ export const EVENT_NAMES = {
     ROUTER_CAPABILITIES: "routerCapabilities",
     CREATE_TRANSPORT: "createTransport",
     TRANSPORT_CREATED: "transportCreated",
+    CONNECT_PRODUCER_TRANSPORT: "connectProducerTransport",
+    CONNECT_CONSUMER_TRANSPORT: "connectConsumerTransport",
+    TRANSPORT_PRODUCE: "transportProduce",
+    CONSUME_MEDIA: "consumeMedia",
+    CONSUME_RESUME: "consumeResume",
 };
